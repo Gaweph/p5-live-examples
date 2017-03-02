a9b57a3633ca00555662a19e25705404baf693c9
Gareth Williams <git@randomsyntax.net>
2017-03-02 00:49:36 +0000

    -removed commented out code

--- Fractal_spirograph_p5/code/sketch.ts
+++ Fractal_spirograph_p5/code/sketch.ts
@@ -2,9 +2,9 @@ var p = new p5();
 var resolution = 25;
 
 let fullRotation: number = p.PI * 2;
-let leftStartingAngle = -p.PI;//+ (p.PI/10);
-let rightStartingAngle = (p.PI * 2);// - (p.PI/10);
-let bottomStartingAngle = p.PI/2;// - (p.PI/5);
+let leftStartingAngle = -p.PI+ (p.PI/10);
+let rightStartingAngle = (p.PI * 2) - (p.PI/10);
+let bottomStartingAngle = p.PI/2 - (p.PI/5);
 let bottomLeftStartingAngle = p.PI/2 + (p.PI/5);
 let topStartingAngle = -p.PI/2;
 class Sketch {
@@ -16,25 +16,14 @@ class Sketch {
   }
   setup () {
     var k = 4;
-    var change = ((k + 1) * 2);
-    var leftAngle = leftStartingAngle + (p.PI/change);
-
-    change = ((k + 1) * 2);
-    var rightAngle = rightStartingAngle - (p.PI/change);
-
-    change = ((k + 1));
-    console.log(change);
-    var bottomAngle = bottomStartingAngle - (p.PI/change);
-    
-    var topAngle = topStartingAngle;
     var colors = ColorHelper.getColorsArray(5);
     
     //add more if you like :)
     for(var i = 1; i <= 1; i++) {
-      this.suns.push(new Sun(p.width/4/i, 10/i, -k, leftAngle, colors[0], fullRotation/4));
-      this.suns.push(new Sun(p.width/4/i, 10/i, -k, topAngle, colors[1], fullRotation/4));
-      this.suns.push(new Sun(p.width/4/i, 10/i, -k, rightAngle, colors[2], fullRotation/4));
-      this.suns.push(new Sun(p.width/4/i, 10/i, -k, bottomAngle, colors[3], fullRotation/4));
+      this.suns.push(new Sun(p.width/4/i, 10/i, -k, leftStartingAngle, colors[0], fullRotation/4));
+      this.suns.push(new Sun(p.width/4/i, 10/i, -k, topStartingAngle, colors[1], fullRotation/4));
+      this.suns.push(new Sun(p.width/4/i, 10/i, -k, rightStartingAngle, colors[2], fullRotation/4));
+      this.suns.push(new Sun(p.width/4/i, 10/i, -k, bottomStartingAngle, colors[3], fullRotation/4));
       this.suns.push(new Sun(p.width/4/i, 10/i, -k, bottomLeftStartingAngle, colors[4], fullRotation/4));
     }
   }
