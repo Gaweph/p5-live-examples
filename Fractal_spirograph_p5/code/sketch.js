980d30e2d0193ada915bda86dc8c04b21a504d74
Gareth Williams <git@randomsyntax.net>
2017-03-02 00:31:45 +0000

    -aded a fifth sun

--- Fractal_spirograph_p5/code/sketch.js
+++ Fractal_spirograph_p5/code/sketch.js
@@ -1,20 +1,31 @@
 var p = new p5();
 var resolution = 25;
 let fullRotation = p.PI * 2;
-let leftStartingAngle = -p.PI + (p.PI / 10);
-let rightStartingAngle = (p.PI * 2) - (p.PI / 10);
-let bottomStartingAngle = p.PI / 2 - (p.PI / 5);
+let leftStartingAngle = -p.PI;
+let rightStartingAngle = (p.PI * 2);
+let bottomStartingAngle = p.PI / 2;
+let bottomLeftStartingAngle = p.PI / 2 + (p.PI / 5);
 let topStartingAngle = -p.PI / 2;
 class Sketch {
     constructor() {
         this.suns = [];
     }
     setup() {
+        var k = 4;
+        var change = ((k + 1) * 2);
+        var leftAngle = leftStartingAngle + (p.PI / change);
+        change = ((k + 1) * 2);
+        var rightAngle = rightStartingAngle - (p.PI / change);
+        change = ((k + 1));
+        console.log(change);
+        var bottomAngle = bottomStartingAngle - (p.PI / change);
+        var topAngle = topStartingAngle;
         var colors = ColorHelper.getColorsArray(6);
-        this.suns.push(new Sun(p.width / 4, 10, -4, leftStartingAngle, colors[0], fullRotation / 2));
-        this.suns.push(new Sun(p.width / 4, 10, -4, topStartingAngle, colors[1], fullRotation / 2));
-        this.suns.push(new Sun(p.width / 4, 10, -4, rightStartingAngle, colors[2], fullRotation / 2));
-        this.suns.push(new Sun(p.width / 4, 10, -4, bottomStartingAngle, colors[3], fullRotation / 2));
+        this.suns.push(new Sun(p.width / 4, 10, -k, leftAngle, colors[0], fullRotation / 4));
+        this.suns.push(new Sun(p.width / 4, 10, -k, topAngle, colors[1], fullRotation / 4));
+        this.suns.push(new Sun(p.width / 4, 10, -k, rightAngle, colors[2], fullRotation / 4));
+        this.suns.push(new Sun(p.width / 4, 10, -k, bottomAngle, colors[3], fullRotation / 4));
+        this.suns.push(new Sun(p.width / 4, 10, -k, bottomLeftStartingAngle, colors[5], fullRotation / 4));
     }
     draw() {
         p.background(51);
--- Fractal_spirograph_p5/code/sketch.ts
+++ Fractal_spirograph_p5/code/sketch.ts
@@ -2,9 +2,10 @@ var p = new p5();
 var resolution = 25;
 
 let fullRotation: number = p.PI * 2;
-let leftStartingAngle = -p.PI + (p.PI/10);
-let rightStartingAngle = (p.PI * 2) - (p.PI/10);
-let bottomStartingAngle = p.PI/2 - (p.PI/5);
+let leftStartingAngle = -p.PI;//+ (p.PI/10);
+let rightStartingAngle = (p.PI * 2);// - (p.PI/10);
+let bottomStartingAngle = p.PI/2;// - (p.PI/5);
+let bottomLeftStartingAngle = p.PI/2 + (p.PI/5);
 let topStartingAngle = -p.PI/2;
 class Sketch {
 
@@ -14,11 +15,26 @@ class Sketch {
 
   }
   setup () {
+    var k = 4;
+    var change = ((k + 1) * 2);
+    var leftAngle = leftStartingAngle + (p.PI/change);
+
+    change = ((k + 1) * 2);
+    var rightAngle = rightStartingAngle - (p.PI/change);
+
+    change = ((k + 1));
+    console.log(change);
+    var bottomAngle = bottomStartingAngle - (p.PI/change);
+    
+    var topAngle = topStartingAngle;
+
     var colors = ColorHelper.getColorsArray(6);
-    this.suns.push(new Sun(p.width/4, 10, -4, leftStartingAngle, colors[0], fullRotation/2));
-    this.suns.push(new Sun(p.width/4, 10, -4, topStartingAngle, colors[1], fullRotation/2));
-    this.suns.push(new Sun(p.width/4, 10, -4, rightStartingAngle, colors[2], fullRotation/2));
-    this.suns.push(new Sun(p.width/4, 10, -4, bottomStartingAngle, colors[3], fullRotation/2));
+    this.suns.push(new Sun(p.width/4, 10, -k, leftAngle, colors[0], fullRotation/4));
+    this.suns.push(new Sun(p.width/4, 10, -k, topAngle, colors[1], fullRotation/4));
+     this.suns.push(new Sun(p.width/4, 10, -k, rightAngle, colors[2], fullRotation/4));
+    this.suns.push(new Sun(p.width/4, 10, -k, bottomAngle, colors[3], fullRotation/4));
+    this.suns.push(new Sun(p.width/4, 10, -k, bottomLeftStartingAngle, colors[5], fullRotation/4));
+    
     // this.suns.push(new Sun(p.width/4, 10, -4, rightStartingAngle, colors[1], fullRotation/2));
     // this.suns.push(new Sun(p.width/4, 10, -4, bottomStartingAngle, colors[2], fullRotation/2));
     // this.suns.push(new Sun(p.width/5, 8, -3, rightStartingAngle, colors[1], fullRotation/2));
