ff49ec95af8fe349e289cc35bbec9b527cc13ccd
Gareth Williams <github@randomsyntax.net>
2018-01-13 23:56:44 +0000

    -aded row color functionality

--- CC_86_beesandbombs/code/sketch.js
+++ CC_86_beesandbombs/code/sketch.js
@@ -1,38 +1,51 @@
 class myP5 extends p5 {
 }
 var p = new myP5();
+let canvasWidth = 400;
+let canvasHeight = 400;
 let angle = 0;
 let w = 24;
 let ma;
 let maxD;
+let minHeight = 100;
+let maxHeight = 300;
 function setup() {
-    p.createCanvas(400, 400, p.WEBGL);
+    p.createCanvas(canvasWidth, canvasHeight, p.WEBGL);
     ma = p.atan(p.cos(p.QUARTER_PI));
     maxD = p.dist(0, 0, 200, 200);
 }
 function draw() {
-    p.colorMode(p.RGB);
     p.background(200);
     p.ortho(-400, 400, 400, -400, 0, 1000);
     p.rotateX(-ma);
     p.rotateY(-p.QUARTER_PI);
-    p.colorMode(p.HSB);
     p.pointLight(255, 255, 255, 0, 0, 400);
     p.pointLight(100, 50, 100, -300, -300, p.height / 2);
     p.directionalLight(150, 150, 150, -0.8, -0.8, 0);
+    let cols = (p.width / w);
+    let rows = (p.height / w);
+    let heightsColors = ColorHelper.getColorsArray(200);
+    let rowColors = ColorHelper.getColorsArray(rows);
+    var count = 0;
     for (let z = 0; z < p.height; z += w) {
         for (let x = 0; x < p.width; x += w) {
             p.push();
             let d = p.dist(x, z, p.width / 2, p.height / 2);
             let offset = p.map(d, 0, maxD, -p.PI, p.PI);
             let a = angle + offset;
-            let h = p.floor(p.map(p.sin(a), -1, 1, 100, 300));
+            let h = mapHeight(a);
             p.translate(x - p.width / 2, 0, z - p.height / 2);
-            p.ambientMaterial(210);
+            let row = p.floor(z / w);
+            let rowColor = rowColors[row];
+            let heightColor = heightsColors[h - 100];
+            p.ambientMaterial(rowColor);
             p.box(w, h, w);
             p.pop();
         }
     }
     angle -= 0.1;
 }
+var mapHeight = (angle) => {
+    return p.floor(p.map(p.sin(angle), -1, 1, minHeight, maxHeight));
+};
 //# sourceMappingURL=sketch.js.map
--- CC_86_beesandbombs/code/sketch.ts
+++ CC_86_beesandbombs/code/sketch.ts
@@ -19,19 +19,23 @@ class myP5 extends p5 {
 
 var p: myP5 = new myP5();
 
+let canvasWidth = 400;
+let canvasHeight = 400;
 let angle = 0;
 let w = 24;
 let ma;
 let maxD;
+let minHeight = 100;
+let maxHeight = 300;
 
 function setup() {
-    p.createCanvas(400, 400, p.WEBGL);
+    p.createCanvas(canvasWidth, canvasHeight, p.WEBGL);
     ma = p.atan(p.cos(p.QUARTER_PI));
     maxD = p.dist(0, 0, 200, 200);
 }
 
 function draw() {
-    p.colorMode(p.RGB);
+    // p.colorMode(p.RGB);
     p.background(200);
 
     p.ortho(-400, 400, 400, -400, 0, 1000);
@@ -39,31 +43,41 @@ function draw() {
     p.rotateY(-p.QUARTER_PI);
 
     // lighting copied from Davenewt's variant: https://codepen.io/anon/pen/wprwdP?editors=0010
-    p.colorMode(p.HSB);
+    //p.colorMode(p.HSB);
     p.pointLight(255, 255, 255, 0, 0, 400);
     p.pointLight(100, 50, 100, -300, -300, p.height / 2);
     p.directionalLight(150, 150, 150, -0.8, -0.8, 0);
 
+    let cols = (p.width / w);
+    let rows = (p.height / w);
+    let heightsColors = ColorHelper.getColorsArray(200);
+    let rowColors = ColorHelper.getColorsArray(rows);
+    var count = 0;
     for (let z = 0; z < p.height; z += w) {
         for (let x = 0; x < p.width; x += w) {
             p.push();
             let d = p.dist(x, z, p.width / 2, p.height / 2);
             let offset = p.map(d, 0, maxD, -p.PI, p.PI);
             let a = angle + offset;
-            let h = p.floor(p.map(p.sin(a), -1, 1, 100, 300));
+            let h = mapHeight(a);
             (<any>p).translate(x - p.width / 2, 0, z - p.height / 2);
             //p.normalMaterial();
 
-            p.ambientMaterial(210);
-            //p.ambientMaterial(p.color('blue'));
-            //p.ambientMaterial(p.color('green'));
-            //p.ambientMaterial(p.color('red'));
+            let row = p.floor(z / w);
+            let rowColor = rowColors[row];
+            let heightColor = heightsColors[h - 100]
+            p.ambientMaterial(rowColor);
+            //p.ambientMaterial(heightColor);
 
             p.box(w, h, w);
-            //rect(x - width / 2 + w / 2, 0, w - 2, h);
             p.pop();
         }
     }
 
   angle -= 0.1;
 }
+
+
+var mapHeight = (angle) => {
+    return p.floor(p.map(p.sin(angle), -1, 1, minHeight, maxHeight));
+};
