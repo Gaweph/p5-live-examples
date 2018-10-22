6d95718fa13e83f157592f2336277f695b19ca88
Gareth Williams <github@randomsyntax.net>
2018-10-22 23:58:26 +0100

    -variance in angle

--- CodingChallenges/CC_014_FractalTree/code/sketch.js
+++ CodingChallenges/CC_014_FractalTree/code/sketch.js
@@ -5,28 +5,29 @@ var sketch = (p) => {
     p.preload = () => {
     };
     p.setup = () => {
-        p.createCanvas(400, 400);
+        p.createCanvas(p.windowWidth, p.windowHeight);
         sliderAngle = p.createSlider(0, p.TWO_PI, p.PI / 4, 0.01);
     };
     p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
     };
     p.draw = () => {
         p.background(51);
         angle = sliderAngle.value();
         p.stroke(p.color(255));
-        p.translate(200, p.height);
-        growth -= 1;
+        p.translate(p.width / 2, p.height);
+        growth -= 10;
         if (growth < 4) {
             growth = 4;
         }
-        this.branch(100, growth);
+        this.branch(p.height / 3, growth);
     };
     this.branch = (len, threshold) => {
         p.line(0, 0, 0, -len);
         p.translate(0, -len);
         if (len > threshold) {
             p.push();
-            p.rotate(angle);
+            p.rotate(angle / 2);
             this.branch(len * 0.67, threshold);
             p.pop();
             p.push();
--- CodingChallenges/CC_014_FractalTree/code/sketch.ts
+++ CodingChallenges/CC_014_FractalTree/code/sketch.ts
@@ -14,26 +14,26 @@ var sketch = (p: p5) => {
   }
   
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(p.windowWidth, p.windowHeight);
     sliderAngle = p.createSlider(0, p.TWO_PI, p.PI / 4, 0.01);
     // sliderBranches = p.createSlider(1, 50, 4);
   }
   
   p.windowResized = () => {
-      // p.resizeCanvas(p.windowWidth, p.windowHeight);
+      p.resizeCanvas(p.windowWidth, p.windowHeight);
   }
   
   p.draw = () => {
     p.background(51);
     angle = sliderAngle.value();
     p.stroke(p.color(255));
-    p.translate(200, p.height);
+    p.translate(p.width/2, p.height);
     // var threshold = sliderBranches.value();
-    growth -= 1;
+    growth -= 10;
     if(growth < 4) {
       growth = 4;
     }
-    this.branch(100, growth);   
+    this.branch(p.height/3, growth);   
   }
 
   this.branch = (len: number, threshold: number) => {
@@ -41,7 +41,7 @@ var sketch = (p: p5) => {
     p.translate(0, -len);
     if (len > threshold) {
       p.push();
-      p.rotate(angle);
+      p.rotate(angle / 2);
       this.branch(len * 0.67, threshold);
       p.pop();
       p.push();
