7492a177eaed8ffe4b172276e535ce9bd65df426
Gareth Williams <g.v.williams@swansea.ac.uk>
2019-05-03 17:08:29 +0100

    -hello world

--- Random/TextSticks/build/build.js
+++ Random/TextSticks/build/build.js
@@ -1,43 +1,26 @@
-var data;
-var src;
-function setup() {
-    src = loadImage("/public/wallpaper-798984.jpg");
-    createCanvas(displayWidth, displayHeight);
-    data = [];
-    for (var i = 0; i < width; i++) {
-        data[i] = [];
-        for (var j = 0; j < height; j++) {
-            data[i][j] = 0;
-        }
-    }
-    genNoise(data);
-    background(0);
-    src.resize(width, height);
+var font;
+function preload() {
+    font = loadFont('/public/Digitalt.ttf');
 }
-function genNoise(data) {
-    var noiseScale = 0.0075;
-    noiseDetail(1, 0.5);
-    for (var i = 0; i < data.length; i++) {
-        for (var j = 0; j < data[i].length; j++) {
-            data[i][j] = noise(j * noiseScale, i * noiseScale);
-        }
-    }
+var points;
+var bounds;
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    stroke(0);
+    fill(255, 104, 204);
+    points = font.textToPoints('Hello World', 0, 0, 100, {
+        sampleFactor: 5,
+        simplifyThreshold: 0
+    });
+    bounds = font.textBounds(' Hello World ', 0, 0, 100);
 }
 function draw() {
-    for (var i = 0; i < 100; i++) {
-        var nX = floor(random(width));
-        var nY = floor(random(height));
-        var d = data[nX][nY];
-        push();
-        var c = src.get(nX, nY);
-        fill(c);
-        noStroke();
-        translate(nX, nY);
-        rotate(map(d, 0, 1, -PI, PI));
-        var newLength = map(nY, 0, height, 10, 75);
-        var newWidth = map(nY, 0, height, 3, 10);
-        rectMode(CENTER);
-        rect(0, 0, newLength, newWidth);
-        pop();
+    background(255);
+    beginShape();
+    translate(0, bounds.h);
+    for (var i = 0; i < points.length; i++) {
+        var p = points[i];
+        point(p.x, p.y);
     }
+    endShape(CLOSE);
 }
--- Random/TextSticks/sketch/sketch.ts
+++ Random/TextSticks/sketch/sketch.ts
@@ -1,50 +1,35 @@
-var data: Array<number[]>;
-var src: p5.Image; 
+let font: p5.Font;
+function preload() {
+  font = loadFont('/public/Digitalt.ttf');
+}
 
+let points: p5.Vector[];
+let bounds: any;
 function setup() {
-  src = loadImage("/public/wallpaper-798984.jpg");
   createCanvas(displayWidth, displayHeight);
-  data = [];
+  stroke(0);
+  fill(255, 104, 204);
 
-  for(var i = 0; i < width; i++) {
-    data[i] = [];
-    for(var j = 0; j< height; j++) {
-        data[i][j] = 0;
-    }
-}
-
-  genNoise(data);
-  background(0);
-  src.resize(width, height);
-}
-
-
-
-function genNoise(data: number[][]) {
-  var noiseScale = 0.0075;
-  noiseDetail(1, 0.5);
-  for (var i = 0; i < data.length; i++) {
-    for (var j = 0; j < data[i].length; j++) {
-      data[i][j] = noise(j * noiseScale, i * noiseScale);
-    }
-  }
+  points = font.textToPoints('Hello World', 0, 0, 100, {
+    sampleFactor: 5,
+    simplifyThreshold: 0
+  });
+  bounds = font.textBounds(' Hello World ', 0, 0, 100);
 }
 
 function draw() {
-  for (var i = 0; i < 100; i++) {
-    var nX = floor(random(width));
-    var nY = floor(random(height));
-    var d = data[nX][nY];
-    push();
-        var c = <p5.Color>src.get(nX, nY);
-        fill(c);
-        noStroke();
-        translate(nX, nY);
-        rotate(map(d, 0, 1, -PI, PI));
-        var newLength = map(nY, 0, height, 10, 75);
-        var newWidth = map(nY, 0, height, 3, 10);
-        rectMode(CENTER);
-        rect(0, 0, newLength, newWidth);
-    pop();
+  background(255);
+  beginShape();
+  translate(0, bounds.h);
+  // translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
+  for (let i = 0; i < points.length; i++) {
+    let p = points[i];
+    point(p.x,p.y);
+    // vertex(
+    //   p.x * width / bounds.w +
+    //     sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
+    //   p.y * height / bounds.h
+    // );
   }
+  endShape(CLOSE);
 }
