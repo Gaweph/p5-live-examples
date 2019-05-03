522ffb929ccaa9110d2f5e2bc399b36ab69671ef
Gareth Williams <g.v.williams@swansea.ac.uk>
2019-05-03 17:36:52 +0100

    -trying to do intersection

--- Random/TextSticks/build/build.js
+++ Random/TextSticks/build/build.js
@@ -4,23 +4,29 @@ function preload() {
 }
 var points;
 var bounds;
+var pairs;
 function setup() {
     createCanvas(displayWidth, displayHeight);
     stroke(0);
     fill(255, 104, 204);
-    points = font.textToPoints('Hello World', 0, 0, 100, {
+    bounds = font.textBounds('A', 0, 0, 200);
+    points = font.textToPoints('A', 0, 0, 200, {
         sampleFactor: 5,
         simplifyThreshold: 0
     });
-    bounds = font.textBounds(' Hello World ', 0, 0, 100);
 }
 function draw() {
     background(255);
-    beginShape();
-    translate(0, bounds.h);
-    for (var i = 0; i < points.length; i++) {
-        var p = points[i];
-        point(p.x, p.y);
+    translate(100, 100 + bounds.h);
+    stroke(10);
+    for (var i = 0; i < 100; i++) {
+        line(i + bounds.x, -10 + -bounds.h, i + bounds.x, 10);
     }
-    endShape(CLOSE);
+    stroke(200);
+    for (var i_1 = 0; i_1 < points.length; i_1++) {
+        var p = points[i_1];
+        beginShape();
+        vertex(p.x, p.y);
+        endShape(CLOSE);
+    }
 }
--- Random/TextSticks/sketch/sketch.ts
+++ Random/TextSticks/sketch/sketch.ts
@@ -5,31 +5,65 @@ function preload() {
 
 let points: p5.Vector[];
 let bounds: any;
+var pairs: {a: p5.Vector, b: p5.Vector}[];
 function setup() {
   createCanvas(displayWidth, displayHeight);
   stroke(0);
   fill(255, 104, 204);
 
-  points = font.textToPoints('Hello World', 0, 0, 100, {
+  bounds = font.textBounds('A', 0, 0, 200);
+  
+  points = font.textToPoints('A', 0, 0, 200, {
     sampleFactor: 5,
     simplifyThreshold: 0
   });
-  bounds = font.textBounds(' Hello World ', 0, 0, 100);
+
+  // pairs = [];
+  // // put all points into pairs
+  // for(var i = 0; i < points.length; i++) {
+
+  //   var p = points[i];
+  //   var x = p.x;
+  //   var y = p.x;
+
+  //   // look for matching 
+  //   var matches = points.filter( obj => obj.x === x && obj.y < y );
+  //   if(matches.length > 0) {
+  //     var match = matches[0];
+  //     pairs[pairs.length] = {a : p, b : match};
+  //   }
+
+  // }
+
+
 }
 
 function draw() {
   background(255);
-  beginShape();
-  translate(0, bounds.h);
-  // translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
+  
+  translate(100, 100 + bounds.h);
+
+  // rect(bounds.x, bounds.y, bounds.w,bounds.h);
+  
+  stroke(10);
+  for(var i = 0; i < 100; i++ ){
+    line(i + bounds.x, -10 + -bounds.h, i + bounds.x, 10);
+  }
+  stroke(200);
   for (let i = 0; i < points.length; i++) {
     let p = points[i];
-    point(p.x,p.y);
+    // 
+    //line(p.a.x,p.a.y, p.b.x, p.b.y);
+    beginShape();
+    // point(p.x,p.y);
+    vertex(p.x,p.y);
+    endShape(CLOSE);
     // vertex(
     //   p.x * width / bounds.w +
     //     sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
     //   p.y * height / bounds.h
     // );
   }
-  endShape(CLOSE);
+
+
 }
