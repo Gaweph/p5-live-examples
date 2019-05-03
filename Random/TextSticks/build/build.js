de27c735489310978c954ad5b4d0b713f00873e0
Gareth Williams <github@randomsyntax.net>
2019-05-03 23:10:47 +0100

    try to determine point inside shape

--- Random/TextSticks/build/build.js
+++ Random/TextSticks/build/build.js
@@ -11,22 +11,84 @@ function setup() {
     fill(255, 104, 204);
     bounds = font.textBounds('A', 0, 0, 200);
     points = font.textToPoints('A', 0, 0, 200, {
-        sampleFactor: 5,
+        sampleFactor: 1,
         simplifyThreshold: 0
     });
+    polyCorners = points.length;
+    polyX = points.map(function (p) { return p.x; });
+    polyY = points.map(function (p) { return p.y; });
+    precalc_values();
 }
 function draw() {
     background(255);
+    push();
     translate(100, 100 + bounds.h);
-    stroke(10);
-    for (var i = 0; i < 100; i++) {
-        line(i + bounds.x, -10 + -bounds.h, i + bounds.x, 10);
-    }
-    stroke(200);
+    beginShape();
+    strokeWeight(1);
     for (var i_1 = 0; i_1 < points.length; i_1++) {
         var p = points[i_1];
-        beginShape();
         vertex(p.x, p.y);
-        endShape(CLOSE);
     }
+    endShape(CLOSE);
+    strokeWeight(5);
+    for (var i = 0; i < 10; i++) {
+        point(i * 10, -i * 10);
+        var tmp = pointInPolygonx(1 * 10, -i * 10);
+        if (tmp) {
+            console.log(tmp);
+        }
+    }
+    pop();
+}
+function pointInPolygonx(x, y) {
+    var i = polyCorners - 1;
+    var j = polyCorners - 1;
+    var oddNodes = false;
+    for (i = 0; i < polyCorners; i++) {
+        if ((polyY[i] < y && polyY[j] >= y
+            || polyY[j] < y && polyY[i] >= y)
+            && (polyX[i] <= x || polyX[j] <= x)) {
+            oddNodes || (polyX[i] + (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i]) < x);
+        }
+        j = i;
+    }
+    return oddNodes;
+}
+var polyCorners;
+var polyX;
+var polyY;
+var constant;
+var multiple;
+function precalc_values() {
+    constant = [];
+    multiple = [];
+    var i = polyCorners - 1;
+    var j = polyCorners - 1;
+    for (i = 0; i < polyCorners; i++) {
+        if (polyY[j] == polyY[i]) {
+            constant[i] = polyX[i];
+            multiple[i] = 0;
+        }
+        else {
+            constant[i] = polyX[i] - (polyY[i] * polyX[j]) / (polyY[j] - polyY[i]) + (polyY[i] * polyX[i]) / (polyY[j] - polyY[i]);
+            multiple[i] = (polyX[j] - polyX[i]) / (polyY[j] - polyY[i]);
+        }
+        j = i;
+    }
+}
+function pointInPolygon(x, y) {
+    var polyCorners = points.length;
+    var polyX = points.map(function (p) { return p.x; });
+    var polyY = points.map(function (p) { return p.y; });
+    var i = polyCorners - 1;
+    var j = polyCorners - 1;
+    var oddNodes = false;
+    for (i = 0; i < polyCorners; i++) {
+        if ((polyY[i] < y && polyY[j] >= y
+            || polyY[j] < y && polyY[i] >= y)) {
+            oddNodes || (y * multiple[i] + constant[i] < x);
+        }
+        j = i;
+    }
+    return oddNodes;
 }
--- Random/TextSticks/sketch/sketch.ts
+++ Random/TextSticks/sketch/sketch.ts
@@ -5,65 +5,143 @@ function preload() {
 
 let points: p5.Vector[];
 let bounds: any;
-var pairs: {a: p5.Vector, b: p5.Vector}[];
+var pairs: { a: p5.Vector, b: p5.Vector }[];
 function setup() {
   createCanvas(displayWidth, displayHeight);
   stroke(0);
   fill(255, 104, 204);
 
   bounds = font.textBounds('A', 0, 0, 200);
-  
+
   points = font.textToPoints('A', 0, 0, 200, {
-    sampleFactor: 5,
+    sampleFactor: 1,
     simplifyThreshold: 0
   });
 
-  // pairs = [];
-  // // put all points into pairs
-  // for(var i = 0; i < points.length; i++) {
 
-  //   var p = points[i];
-  //   var x = p.x;
-  //   var y = p.x;
+  polyCorners = points.length;
+  polyX = points.map(p => p.x);
+  polyY = points.map(p => p.y);
 
-  //   // look for matching 
-  //   var matches = points.filter( obj => obj.x === x && obj.y < y );
-  //   if(matches.length > 0) {
-  //     var match = matches[0];
-  //     pairs[pairs.length] = {a : p, b : match};
-  //   }
-
-  // }
-
-
+  precalc_values();
 }
 
 function draw() {
   background(255);
-  
-  translate(100, 100 + bounds.h);
 
-  // rect(bounds.x, bounds.y, bounds.w,bounds.h);
-  
-  stroke(10);
-  for(var i = 0; i < 100; i++ ){
-    line(i + bounds.x, -10 + -bounds.h, i + bounds.x, 10);
-  }
-  stroke(200);
+  push();
+  translate(100, 100 + bounds.h);
+  beginShape();
+  strokeWeight(1);
   for (let i = 0; i < points.length; i++) {
     let p = points[i];
-    // 
-    //line(p.a.x,p.a.y, p.b.x, p.b.y);
-    beginShape();
-    // point(p.x,p.y);
-    vertex(p.x,p.y);
-    endShape(CLOSE);
-    // vertex(
-    //   p.x * width / bounds.w +
-    //     sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
-    //   p.y * height / bounds.h
-    // );
+    vertex(p.x, p.y);
+  }
+  endShape(CLOSE);
+
+  strokeWeight(5);
+
+  for (var i = 0; i < 10; i++) {
+    point(i * 10, - i * 10);
+    var tmp = pointInPolygonx(1 * 10, -i * 10);
+    if (tmp) {
+      console.log(tmp);
+    }
   }
 
+  pop();
+}
 
+function pointInPolygonx(x: number, y: number) {
+
+
+  var i = polyCorners - 1;
+  var j = polyCorners - 1;
+
+  var oddNodes = false;
+
+  for (i = 0; i < polyCorners; i++) {
+    if ((polyY[i] < y && polyY[j] >= y
+      || polyY[j] < y && polyY[i] >= y)
+      && (polyX[i] <= x || polyX[j] <= x)) {
+      oddNodes || (polyX[i] + (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i]) < x);
+    }
+    j = i;
+  }
+
+  return oddNodes;
+}
+
+//  Globals which should be set before calling these functions:
+//
+//  int    polyCorners  =  how many corners the polygon has (no repeats)
+//  float  polyX[]      =  horizontal coordinates of corners
+//  float  polyY[]      =  vertical coordinates of corners
+//  float  x, y         =  point to be tested
+//
+//  The following global arrays should be allocated before calling these functions:
+//
+//  float  constant[] = storage for precalculated constants (same size as polyX)
+//  float  multiple[] = storage for precalculated multipliers (same size as polyX)
+//
+//  (Globals are used in this example for purposes of speed.  Change as
+//  desired.)
+//
+//  USAGE:
+//  Call precalc_values() to initialize the constant[] and multiple[] arrays,
+//  then call pointInPolygon(x, y) to determine if the point is in the polygon.
+//
+//  The function will return YES if the point x,y is inside the polygon, or
+//  NO if it is not.  If the point is exactly on the edge of the polygon,
+//  then the function may return YES or NO.
+//
+//  Note that division by zero is avoided because the division is protected
+//  by the "if" clause which surrounds it.
+
+
+var polyCorners: number;
+var polyX: number[];
+var polyY: number[];
+var constant: number[];
+var multiple: number[];
+function precalc_values() {
+
+  constant = [];
+  multiple = [];
+
+  var i = polyCorners - 1;
+  var j = polyCorners - 1;
+
+  for (i = 0; i < polyCorners; i++) {
+    if (polyY[j] == polyY[i]) {
+      constant[i] = polyX[i];
+      multiple[i] = 0;
+    }
+    else {
+      constant[i] = polyX[i] - (polyY[i] * polyX[j]) / (polyY[j] - polyY[i]) + (polyY[i] * polyX[i]) / (polyY[j] - polyY[i]);
+      multiple[i] = (polyX[j] - polyX[i]) / (polyY[j] - polyY[i]);
+    }
+    j = i;
+  }
+}
+
+function pointInPolygon(x: number, y: number): boolean {
+
+  var polyCorners = points.length;
+  var polyX = points.map(p => p.x);
+  var polyY = points.map(p => p.y);
+
+  var i = polyCorners - 1;
+  var j = polyCorners - 1;
+  var oddNodes = false;
+
+  for (i = 0; i < polyCorners; i++) {
+    if ((polyY[i] < y && polyY[j] >= y
+      || polyY[j] < y && polyY[i] >= y)) {
+      oddNodes || (y * multiple[i] + constant[i] < x);
+    }
+    j = i;
+  }
+
+  return oddNodes;
 }
